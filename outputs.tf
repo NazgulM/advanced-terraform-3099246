2ec7c0d7ad2d1e91783509e6d6d8723b98f9fd9b
NazgulM <maniyasova.n@gmail.com>
2023-11-04 20:51:45 -0500

    looping

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,24 @@
 ### OUTPUTS ###
-output "nginx-public-ip" {
-    value = google_compute_instance.nginx_instance.network_interface[0].access_config[0].nat_ip
-}
+# output "nginx-public-ip" {
+#     value = google_compute_instance.nginx_instance.network_interface[0].access_config[0].nat_ip
+# }
 
-output "web1-private-ip" {
-    value = google_compute_instance.web1.network_interface[0].network_ip
-}
+# output "web1-private-ip" {
+#     value = google_compute_instance.web1.network_interface[0].network_ip
+# }
 
-output "web2-private-ip" {
-    value = google_compute_instance.web2.network_interface[0].network_ip
-}
+# output "web2-private-ip" {
+#     value = google_compute_instance.web2.network_interface[0].network_ip
+# }
 
-output "web3-private-ip" {
-    value = google_compute_instance.web3.network_interface[0].network_ip
-}
+# output "web3-private-ip" {
+#     value = google_compute_instance.web3.network_interface[0].network_ip
+# }
+
+# output "db-private-ip" {
+#     value = google_compute_instance.mysqldb.network_interface[0].network_ip
+# }
 
 output "db-private-ip" {
-    value = google_compute_instance.mysqldb.network_interface[0].network_ip
-}
+    value = google_compute_instance.web-instances[*].network_interface[0].network_ip
+ }
